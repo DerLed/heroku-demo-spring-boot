54b651a93a9ccadb8c3c4be2189686afff638a8a
DerLed <48630545+DerLed@users.noreply.github.com>
2022-08-01 21:30:01 +0300

    r

--- src/main/java/com/example/herokudemospringboot/TestController.java
+++ src/main/java/com/example/herokudemospringboot/TestController.java
@@ -1,9 +1,7 @@
 package com.example.herokudemospringboot;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,10 @@ public class TestController {
                 .text("Hello" + count).build();
         messageRepo.save(m);
         return messageRepo.findAll();
+    }
+
+    @PostMapping
+    public Message newMessage(@RequestBody Message message){
+        return messageRepo.save(message);
     }
 }
