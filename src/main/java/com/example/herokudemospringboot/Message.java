e69fd967faeecd883108a26247f0d6a2c1b12a6e
DerLed <48630545+DerLed@users.noreply.github.com>
2022-08-01 21:41:38 +0300

    r

--- src/main/java/com/example/herokudemospringboot/Message.java
+++ src/main/java/com/example/herokudemospringboot/Message.java
@@ -1,10 +1,15 @@
 package com.example.herokudemospringboot;
 
 import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "message")
 @NoArgsConstructor
 @Builder
@@ -17,4 +22,14 @@ public class Message {
     private Long id;
 
     private String text;
+
+
+    private LocalDateTime createTime;
+
+    @PrePersist
+    private void create(){
+        if(createTime == null) {
+            this.createTime = LocalDateTime.now();
+        }
+    }
 }
