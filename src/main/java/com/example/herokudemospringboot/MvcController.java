cace0995e974a12d0b9135e461aeaae0b784ec24
Konstruktor <lediburit@mail.ru>
2022-08-02 09:47:31 +0300

    c

--- src/main/java/com/example/herokudemospringboot/MvcController.java
+++ src/main/java/com/example/herokudemospringboot/MvcController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +19,11 @@ public class MvcController {
     public String index(Model model){
         model.addAttribute("messages", messageRepo.findAll());
         return "index";
+    }
+
+    @PostMapping("/message/del/{id}")
+    public String delete(@PathVariable Long id, Model model){
+        messageRepo.deleteById(id);
+        return "redirect:/index";
     }
 }
